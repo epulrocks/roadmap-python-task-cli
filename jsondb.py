6f820f605405fd9e27ebee458a290d61c0a34a92
Saiful Rahman <epulrocks@gmail.com>
2024-09-12 19:45:37 +0800

    added feature to update task description; organized class static method

--- jsondb.py
+++ jsondb.py
@@ -4,15 +4,23 @@ import time
 class TaskDB:
     def __init__(self, db_path):
         self.db_path = db_path
+        self.tasks = None
+        self.__load_db()
+
+    def __load_db(self):
         try:
             with open(self.db_path, "r") as f:
                 json_dat = json.load(f)
                 self.tasks = json_dat.get("tasks", {})
         except FileNotFoundError:
             self.tasks = {}
-            self.save_db()        
+            self.__save_db()
+    
+    @staticmethod
+    def __time_now():
+        return time.time()
 
-    def save_db(self):
+    def __save_db(self):
         with open(self.db_path, "w") as f:
             json.dump({"tasks": self.tasks}, f)
 
@@ -20,7 +28,7 @@ class TaskDB:
         acceptable_status = ["todo", "in progress", "done"]
         if status not in acceptable_status:
             raise ValueError(f"Invalid status: {status}. Acceptable values are: {acceptable_status}")
-        now = time.time()
+        now = self.__time_now()
         id = len(self.tasks) + 1
         while id in self.tasks:
             id += 1
@@ -31,6 +39,16 @@ class TaskDB:
             "updatedAt": now
         }
         self.tasks[id] = new_task
-        self.save_db()
+        self.__save_db()
         print(f"Task added successfully (ID: {id})")
+        return id
+    
+    def update_task(self, id, description):
+        if id not in self.tasks:
+            raise Exception(f"ID: {id} is not in database")
+        now = self.__time_now()
+        self.tasks[id]['description'] = description
+        self.tasks[id]['updatedAt'] = now
+        self.__save_db()
+        print(f"Task updated successfully (ID: {id})")
         return id
--- task-cli.py
+++ task-cli.py
@@ -7,8 +7,12 @@ db = TaskDB("task-db.json")
 def add(*args):
     db.add_task(*args)
 
+def update(*args):
+    db.update_task(*args)
+
 command_map = {
     "add": add,
+    "update": update,
 }
 
 args = sys.argv[1:]
